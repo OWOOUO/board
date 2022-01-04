dde49b964970a0c38c5ba79cdfd9425dc8e8d5bc
JaeHunKim <ggwogns@gmail.com>
2022-01-04 11:11:58 +0900

    0104 PORTFOLIO

--- daily/urls.py
+++ daily/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+app_name = 'daily'
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('/login', views.login, name='login'),
+    path('/join', views.join, name='join'),
 ]
